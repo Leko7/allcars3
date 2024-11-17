4ebdfa105003757bc3c99b74e2614f7266855060
ilsebbo <sebastiano.scardera@gmail.com>
2024-11-17 21:46:12 +0100

    Added spider to crawl images

--- allcars/spiders/spiders.py
+++ allcars/spiders/spiders.py
@@ -1,5 +1,6 @@
 import json
 import scrapy
+import os
 
 class CarsSpider(scrapy.Spider):
     name = "cars"
@@ -37,6 +38,72 @@ class CarsSpider(scrapy.Spider):
                 car_data[header.strip()] = value.strip() if value else None
         
         yield car_data
+
+class CarsSpiderImage(scrapy.Spider):
+    name = "cars_image"
+    custom_settings = {
+        'DOWNLOAD_DELAY': 3,  # Wait 3 seconds between requests to avoid overloading the server
+    }
+
+    def start_requests(self):
+        # Load URLs from the JSONL file
+        cars_links_path = "data/car_links.jsonl"
+        with open(cars_links_path, "r") as f:
+            for line in f:
+                data = json.loads(line)
+                url = data.get("link")
+                if url:
+                    yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        car_data = {}
+
+        # Extract the Brand name
+        car_data['Brand'] = response.xpath('//tr[th[contains(text(), "Brand")]]/td/a/text()').get()
+
+        # Extract the Model name
+        car_data['Model'] = response.xpath('//tr[th[contains(text(), "Model")]]/td/a/text()').get()
+        
+        # Extract the generation name
+        car_data['Generation'] = response.xpath('//tr[th[contains(text(), "Generation")]]/td/a/text()').get()
+
+        # list of all images in the page
+        lst = response.css('img::attr(src)').getall()
+
+        if lst:
+            # list of cars images
+            car_images = [x.replace('_thumb', '') for x in lst if 'images' in x]
+            car_images = [x.replace('small', 'big') for x in car_images if 'images' in x]
+
+            # Extract the car model name from the URL
+            subdir_name = car_data['brand'] + '_' + car_data['model'] + '_' + car_data['generation']
+
+            car_data['image_path'] = 'temp_images/' + subdir_name
+
+            for num,img_url in enumerate(car_images):
+
+                full_url = response.urljoin(img_url)
+                
+                yield scrapy.Request(
+                        url=full_url,
+                        callback=self.save_image,
+                        cb_kwargs={'dir_name': subdir_name + '/' + str(num)}  # Pass directory path
+                    )
+        else:
+            car_data['image_path'] = None
+        
+        
+        yield car_data
+    
+    def save_image(self, response, dir_name):
+        # Extract image name from URL
+        file_name = response.url.split('/')[-2:]
+        ext = file_name[-1].split('.')[1]
+        # Save the file to a temporary folder
+        save_path = f'temp_images/{dir_name}' + '.' + ext
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+        with open(save_path, 'wb') as f:
+            f.write(response.body)
 
 class CarLinkSpider(scrapy.Spider):
     name = "carlinks"
